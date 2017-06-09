31c808abc2132a580cf03770d4a6a2e68c5e3053
Hans Schoenburg <hschoenburg@gmail.com>
2017-06-09 13:55:29 -0700

    listen on herokus dynamic ports

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 var app = require('./app')
 
-app.listen(3000, function(e) {
-  console.log('Server listening on 3000');
+var port_number = process.env.PORT || 3000;
+
+app.listen(port_number, function(e) {
+  console.log('Server listening on '+port_number)
 });
